2f7ab5ab5619ddc78a7bc81aeb85f52c68727496
azhari hsb <azhari.hsb27@gmail.com>
2025-07-10 12:51:05 +0700

    Implement Home Page Part 2

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,5 +1,27 @@
+import Image from 'next/image';
+import { Button } from './ui/button';
+import Link from 'next/link';
+
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <section className="cta-section">
+      <div className="cta-badge">Start learning your way.</div>
+      <h2 className="text-3xl font-bold">
+        Build and Personalize Learning Companion
+      </h2>
+      <p>
+        Pick a name, subject, voice, & personality - and start learning through
+        voice conversation that feels natural and fun
+      </p>
+      <Image src="/images/cta.svg" alt="CTA" width={362} height={232} />
+      <Button>
+        <Image src="/icons/plus.svg" alt="Plus" width={12} height={12} />
+        <Link href="/companion/new">
+          <p>Build a New Companion</p>
+        </Link>
+      </Button>
+    </section>
+  );
 };
 
 export default CTA;
--- components/CompanionList.tsx
+++ components/CompanionList.tsx
@@ -1,5 +1,94 @@
-const CompanionList = () => {
-  return <div>CompanionList</div>;
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { cn, getSubjectColor } from '@/lib/utils';
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface CompanionsListProps {
+  title: string;
+  companions?: Companion[];
+  classNames?: string;
+}
+
+const CompanionList = ({ companions, classNames }: CompanionsListProps) => {
+  return (
+    <article className={cn('companion-list', classNames)}>
+      <h2 className="text-3xl font-bold">Recent Sessions</h2>
+
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-2/3 text-lg">Lessons</TableHead>
+            <TableHead className="text-lg">Subject</TableHead>
+            <TableHead className="text-lg text-right">Duration</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {companions?.map(({ id, subject, name, topic, duration }) => (
+            <TableRow key={id}>
+              <TableCell>
+                <Link href={`/companions/${id}`}>
+                  <div className="flex items-center gap-2">
+                    <div
+                      className="size-[72px] flex justify-center items-center max-md:hidden rounded-lg"
+                      style={{ backgroundColor: getSubjectColor(subject) }}
+                    >
+                      <Image
+                        src={`/icons/${subject}.svg`}
+                        alt={subject}
+                        width={35}
+                        height={35}
+                      />
+                    </div>
+                    <div className="flex flex-col gap-2">
+                      <p className="text-2xl font-bold">{name}</p>
+                      <p className="text-lg">{topic}</p>
+                    </div>
+                  </div>
+                </Link>
+              </TableCell>
+              <TableCell>
+                <div className="subject-badge w-fit max-md:hidden">
+                  {subject}
+                </div>
+                <div
+                  className="w-fit p-2 flex justify-center items-center md:hidden rounded-lg"
+                  style={{ backgroundColor: getSubjectColor(subject) }}
+                >
+                  <Image
+                    src={`/icons/${subject}.svg`}
+                    alt={subject}
+                    width={18}
+                    height={18}
+                  />
+                </div>
+              </TableCell>
+              <TableCell>
+                <div className="w-full flex justify-end items-center gap-2">
+                  <p className="text-2xl">
+                    {duration} <span className="max-md:hidden">minutes</span>
+                  </p>
+                  <Image
+                    src="/icons/clock.svg"
+                    alt="Minutes"
+                    width={14}
+                    height={14}
+                    className="md:hidden"
+                  />
+                </div>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </article>
+  );
 };
 
 export default CompanionList;
